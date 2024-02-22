41b020fd888291ec25914ebdff9498420ac56b82
KapyKun <haborflorin342@gmail.com>
2024-02-22 17:45:15 +0200

    Lab2 Prob1

---
+++ Lab2/Problema1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isPal(const string& str) {
+    int left = 0;
+    int right = str.length() - 1;
+    while (left < right) {
+        if (str[left] != str[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+void generatePal(int length, string prefix = "") {
+    if (length == 0) {
+        if (isPal(prefix)) {
+            cout << prefix << endl;
+        }
+        return;
+    }
+
+    for (char c : {'0', '1', '2'}) {
+        string newPrefix = prefix + c;
+        generatePal(length - 1, newPrefix);
+    }
+}
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        generatePal(i);
+    }
+    return 0;
+}
