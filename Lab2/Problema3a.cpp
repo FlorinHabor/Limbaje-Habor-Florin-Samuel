e2bd12c5575d7eea908de71a699d01ed74fef2a3
KapyKun <haborflorin342@gmail.com>
2024-03-04 08:49:38 +0200

    Problema 3 a

---
+++ Lab2/Problema3a.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void generateStrings(string currentString, int maxLength) {
+    if (currentString.length() == maxLength) {
+        cout << currentString << endl;
+        return;
+    }
+
+    generateStrings(currentString + "a", maxLength);
+    generateStrings(currentString + "b", maxLength);
+}
+
+int main() {
+    int maxLength = 8;
+    cout << "Toate sirurile de lungime maximă:" << endl;
+    generateStrings("", maxLength);
+    return 0;
+}
