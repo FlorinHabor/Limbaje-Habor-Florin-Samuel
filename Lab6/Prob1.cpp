1e43837ef69acb708fb1f62457fe49b528e0fe4f
FlorinHabor <haborflorin342@gmail.com>
2024-03-28 16:42:44 +0200

    Fix

--- Lab6/Prob1.cpp
+++ Lab6/Prob1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     string cod;
     regex regex("^[1-9][0-9]{4}$");
-    while (1) {
+    while (0) {
         cin >> cod;
         if (regex_match(cod, regex))
             cout << "Este cod postal\n";
