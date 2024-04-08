7ae571c70905b869acaf7c713ce21adba5544ba7
FlorinHabor <haborflorin342@gmail.com>
2024-04-08 16:20:10 +0300

    Lab7/8

---
+++ Lab7/Prob2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+struct Option {
+    string name;
+    int buget;
+    int interes;
+    int vreme;
+    int accesibilitate;
+    int experienta;
+    int durata;
+
+    Option(string n, int bu, int in, int vr, int ac, int exp, int dur)
+        : name(n), buget(bu), interes(in), vreme(vr), accesibilitate(ac), experienta(exp), durata(dur) {}
+};
+
+double calculateTotalScore(const Option& option) {
+    double totalScore = option.buget * 0.4 +
+        option.interes * 0.5 +
+        option.vreme * 0.3 +
+        option.accesibilitate * 0.3 +
+        option.experienta * 0.2 +
+        option.durata * 0.1;
+    return totalScore;
+}
+
+int main() {
+    Option optionA("A", 2, 3, 3, 1, 3, 2);
+    Option optionB("B", 3, 2, 2, 2, 2, 1);
+    Option optionC("C", 1, 1, 1, 3, 2, 3);
+
+    double scoreA = calculateTotalScore(optionA);
+    double scoreB = calculateTotalScore(optionB);
+    double scoreC = calculateTotalScore(optionC);
+
+    cout << "Scorul total pentru A: " << scoreA << " - statiune la malul marii" << endl;
+    cout << "Scorul total pentru B: " << scoreB << " - excursie in munti" << endl;
+    cout << "Scorul total pentru C: " << scoreC << " - calatorie intr-un oras istoric" << endl;
+
+    string bestOption;
+    if (scoreA > scoreB && scoreA > scoreC) {
+        bestOption = "A";
+    }
+    else if (scoreB > scoreA && scoreB > scoreC) {
+        bestOption = "B";
+    }
+    else {
+        bestOption = "C";
+    }
+
+    cout << "Optiunea cea mai favorabila este: " << bestOption << endl;
+
+    return 0;
+}
---
+++ Lab8/Prob2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool isValidIBAN(const string& iban) {
+    
+    if (iban.length() < 4) {
+        return false;
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        if (!isupper(iban[i])) {
+            return false;
+        }
+    }
+
+    for (size_t i = 2; i < iban.length(); ++i) {
+        if (!isdigit(iban[i])&&!isupper(iban[i])) {
+            return false;
+        }
+    }
+
+    for (size_t i = iban.length() - 2; i < iban.length(); ++i) {
+        if (!isdigit(iban[i])) {
+            return false;
+        }
+    }    
+
+    string ibanCopy;
+    for (char c : iban) {
+        if (c != ' ') {
+            ibanCopy += c;
+        }
+    }
+    // Verificarea cifrelor de control IBAN (calculul modului 97)
+    ibanCopy = ibanCopy.substr(4) + ibanCopy.substr(0, 4);
+    for (size_t i = 0; i < ibanCopy.length(); ++i) {
+        if (isdigit(ibanCopy[i])) {
+            ibanCopy[i] = ibanCopy[i] - '0';
+        }
+        else {
+            ibanCopy[i] = ibanCopy[i] - 'A' + 10;
+        }
+    }
+
+    int remainder = 0;
+    for (size_t i = 0; i < ibanCopy.length(); ++i) {
+        remainder = (remainder * 10 + ibanCopy[i]) % 97;
+    }
+
+    return remainder == 1;
+}
+
+int main() {
+    string iban;
+    cout << "Introduceti codul IBAN: ";
+    getline(cin, iban);
+
+    if (isValidIBAN(iban)) {
+        cout << "Codul IBAN este valid.\n";
+    }
+    else {
+        cout << "Codul IBAN nu este valid.\n";
+    }
+
+    return 0;
+}
